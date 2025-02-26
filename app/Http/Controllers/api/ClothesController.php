f1c9158c47bb313994302a3be6b81e8413b6e737
Ónodi András <onodiandraspeter@ktch.hu>
2025-02-26 15:42:10 +0100

    Kettő

--- app/Http/Controllers/api/ClothesController.php
+++ app/Http/Controllers/api/ClothesController.php
@@ -5,7 +5,24 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+
 class ClothesController extends Controller
 {
-    //
+    public function getClothes() {
+        $clothes = Cloth::with()->get();
+
+        return $this->sendResponse( ClothesResource::collection( $clothes ), "Betöltés");
+    }
+    public function getCloth( Request $request ){
+        $cloth = Cloth::where( "ruha", $request[ "ruha" ])->first();
+
+        return $this->sendResponse( new ClotchResource( $cloth ), "Betöltve" );
+    }
+
+    public function isAdmin() {
+        if( !Gate::allows( "status" )) {
+            return "status";
+        }
+        return "nem vásárló";
+    }
 }
