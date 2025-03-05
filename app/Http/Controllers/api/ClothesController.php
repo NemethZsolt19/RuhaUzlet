4323dc4e43a6489f084916e4c3d5abd738306897
Ónodi András <onodiandraspeter@ktch.hu>
2025-03-05 13:33:03 +0100

    Három

--- app/Http/Controllers/api/ClothesController.php
+++ app/Http/Controllers/api/ClothesController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 // use App\Models\Material;
 // use App\Models\Type;
 use App\Models\Clothes;
+use App\Http\Resources\Clothes as ClothesResponse;
+use App\Http\Controllers\api\ResponseController;
 
 
 class ClothesController extends Controller
@@ -15,11 +17,11 @@ class ClothesController extends Controller
     public function getClothes() {
         $clothes = Clothes::with("type", "material", "brand")->get();
 
-        return $this->sendResponse( ClothesResource::collection( $clothes ), "Betöltés");
+        // return $this->sendResponse( ClothesResource::collection( $clothes ), "Betöltés");
     }
     public function getCloth( Request $request ){
         $cloth = Cloth::where( "ruha", $request[ "ruha" ])->first();
 
-        return $this->sendResponse( new ClothResource( $cloth ), "Betöltve" );
+        return $this->sendResponse( new ClothesResource( $cloth ), "Betöltve" );
     }
 }
